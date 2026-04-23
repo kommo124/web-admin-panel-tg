5ba9dca670b28bd4a6066bb59c7cb99f0e49b6de
kommo124 <negrilla32@yandex.ru>
2026-04-23 23:12:04 +0300

    feat: add user`s id to db & get all users from db

---
+++ app/TelegramBot/database.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+cursor = conn.cursor()
+
+def init_db():
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS userid (
+        userid INTEGER NOT NULL UNIQUE
+    )
+    ''')
+    conn.commit()
+
+def getAllUsers():
+    cursor.execute("SELECT * FROM userid")
+    rows = cursor.fetchall()
+    return [row[0] for row in rows]
+    
+
+def addUser(user_id: int):
+    cursor.execute("INSERT INTO userid (userid) VALUES (?)", (user_id,))
+    conn.commit()
+    
+
+
