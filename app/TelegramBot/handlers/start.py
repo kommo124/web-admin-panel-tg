a5ee908aed851efb42d0e36f2137e1877f47c17a
kommo124 <negrilla32@yandex.ru>
2026-04-23 23:20:03 +0300

    feat: add a start command handler

---
+++ app/TelegramBot/handlers/start.py
@@ -0,0 +1,14 @@
+from aiogram import Router, types
+from aiogram.filters import Command
+from database import addUser, getAllUsers
+import logging
+
+router = Router()
+
+@router.message(Command('start'))
+async def cmd_start(message: types.Message):
+    userid = message.from_user.id
+    if addUser(userid):
+        print(f'Пользователь: {userid} был успешно занесён в бд')
+    else:
+        print("Что-то пошло не так")
