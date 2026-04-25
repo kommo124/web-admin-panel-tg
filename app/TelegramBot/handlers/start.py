a8fe1bb6139ab92db4e6860905d7561e2662455f
kommo124 <negrilla32@yandex.ru>
2026-04-25 19:49:29 +0300

    add message.answer when user succesfully added to db

--- app/TelegramBot/handlers/start.py
+++ app/TelegramBot/handlers/start.py
@@ -9,6 +9,9 @@ router = Router()
 async def cmd_start(message: types.Message):
     userid = message.from_user.id
     if addUser(userid):
+        await message.answer("Вы успешно зарегестрировались")
         print(f'Пользователь: {userid} был успешно занесён в бд')
     else:
         print("Что-то пошло не так")
+
+
